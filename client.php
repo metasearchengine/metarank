eea762cdd757f5012e31c1d737e0334384f57745
Ayan Chandra <ayanc18@users.noreply.github.com>
2017-04-11 18:09:18 +0530

    changed from array to array of arrays.

--- client.php
+++ client.php
@@ -5,19 +5,19 @@ include 'yahoo-api-call.php';
 include 'config-project.php';
 
 function get_response_google($query){
-	$list= array();
+	$list= array(array());
 	$list = call_google($query,$list);
 return $list;
 }
 
 function get_response_yahoo($query){
-	$list= array();
+	$list= array(array());
 	$list = call_yahoo($query,$list);
 return $list;
 }
 
 function get_response_bing($query){
-	$list= array();
+	$list= array(array());
 	$list = call_bing($query,$list);
 return $list;
 }
